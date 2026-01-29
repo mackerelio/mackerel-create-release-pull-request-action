03ac9ac87c54ad6f4d742a9d2590486146cb173e
yseto <yseto@users.noreply.github.com>
2026-01-29 17:13:22 +0900

    format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,65 +12,70 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default defineConfig([
-    globalIgnores(["**/dist/", "**/lib/", "**/node_modules/", "**/jest.config.js"]),
-    {
-        extends: fixupConfigRules(
-            compat.extends("eslint:recommended", "plugin:import/recommended", "prettier"),
-        ),
+  globalIgnores(["**/dist/", "**/lib/", "**/node_modules/", "**/jest.config.js"]),
+  {
+    extends: fixupConfigRules(
+      compat.extends("eslint:recommended", "plugin:import/recommended", "prettier")
+    ),
 
-        plugins: {
-            jest,
-        },
-
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...jest.environments.globals.globals,
-            },
-        },
-
-        rules: {
-            "consistent-return": "error",
-            "sort-imports": "off",
-
-            "import/order": ["error", {
-                alphabetize: {
-                    order: "asc",
-                },
-            }],
-        },
+    plugins: {
+      jest,
     },
-    {
-        files: ["**/*.ts"],
 
-        extends: fixupConfigRules(compat.extends(
-            "plugin:@typescript-eslint/eslint-recommended",
-            "plugin:@typescript-eslint/recommended",
-            "plugin:import/typescript",
-        )),
-
-        plugins: {
-            "@typescript-eslint": fixupPluginRules(typescriptEslint),
-        },
-
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: "latest",
-            sourceType: "module",
-
-            parserOptions: {
-                project: "./tsconfig.json",
-            },
-        },
-
-        rules: {
-            "@typescript-eslint/no-empty-function": ["off"],
-        },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...jest.environments.globals.globals,
+      },
     },
+
+    rules: {
+      "consistent-return": "error",
+      "sort-imports": "off",
+
+      "import/order": [
+        "error",
+        {
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.ts"],
+
+    extends: fixupConfigRules(
+      compat.extends(
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript"
+      )
+    ),
+
+    plugins: {
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/no-empty-function": ["off"],
+    },
+  },
 ]);
